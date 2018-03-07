9d35246ca9f2be19f278b9bb3e73501bfb48c922
CodeFuGrandmaster <36977159+CodeFuGrandmaster@users.noreply.github.com>
2018-03-07 12:21:50 -0500

    Scripts v0.01
    Currently holds only a handful of JavaScript functions to roll each of the standard die types.

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function d3Roll()
+{
+  let result = Math.floor(Math.random() * 3) +1;
+  return result;
+}
+
+function d4Roll()
+{
+  let result = Math.floor(Math.random() * 4) +1;
+  return result;
+}
+
+function d6Roll()
+{
+  let result = Math.floor(Math.random() * 6) +1;
+  return result;
+}
+
+function d8Roll()
+{
+  let result = Math.floor(Math.random() * 8) +1;
+  return result;
+}
+
+function d10Roll()
+{
+  let result = Math.floor(Math.random() * 10) + 1;
+  return result;
+}
+
+function d12Roll()
+{
+  let result = Math.floor(Math.random() * 12) + 1;
+  return result;
+}
+
+function d20Roll()
+{
+  let result = Math.floor(Math.random() * 20) + 1;
+  return result;
+}
+
+function d100Roll()
+{
+  let result = Math.floor(Math.random() * 100) +1;
+  return result;
+}
